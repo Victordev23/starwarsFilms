2ecf8f7da9ca2383fe0c84f0e427198c60260aca
Victor <victorcostaraujo4@gmail.com>
2024-05-28 19:00:11 -0300

    Add a database model

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+//model
+const Film = mongoose.model('Film',{ 
+  title: String,
+  description: String,
+  image_url:String,
+  trailer_url:String
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
