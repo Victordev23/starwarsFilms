390034c69e87fd7ea732a5d943c194582d545af2
Victor <victorcostaraujo4@gmail.com>
2024-06-04 18:20:09 -0300

    Add film creation endpoint

--- src/server.js
+++ src/server.js
@@ -2,22 +2,31 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
+app.use(express.json())
 const port = 3000
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-
 //model
-const Film = mongoose.model('Film',{ 
+const Film = mongoose.model('Film', {
   title: String,
   description: String,
-  image_url:String,
-  trailer_url:String
+  image_url: String,
+  trailer_url: String
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/', async (req, res) => {
+
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.image_url
+  })
+  
+  await film.save()
+  res.send(film)
 })
 
 app.listen(port, () => {
   console.log('App running')
+  mongoose.connect('mongodb://127.0.0.1:27017/movies')
 })
